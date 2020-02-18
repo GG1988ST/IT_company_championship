4b47ad929f66fb47e175c2972fa8f584f0cadcbf
FAN YI <2461248y@student.gla.ac.uk>
2020-02-18 17:31:28 +0000

    comments model

--- company/models.py
+++ company/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Company(models.Model):
     NAME_MAX_LENGTH=128
@@ -16,3 +17,13 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+class Comments(models.Model):
+    USERNAME_MAX_LENGTH = 128
+    COMMENTS_MAX_LENGTH = 400
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    upload_by = models.CharField(max_length=USERNAME_MAX_LENGTH,unique=True)
+    comments = models.CharField(max_length=COMMENTS_MAX_LENGTH)
+    date = models.DateField(auto_now=False)
+    def __str__(self):
+        return self.title
