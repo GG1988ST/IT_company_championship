7642c9d2e4696f52ea3dde78d0b2a393f6035a47
GG1988ST <1078793292@qq.com>
2020-02-18 17:06:47 +0000

    add view.show_company

--- company/views.py
+++ company/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from company.models import Company
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def show_company(request, company_name_slug):
+
+    context_dict = {}
+
+    try:
+        company = Company.objects.get(slug=company_name_slug)
+        context_dict['company'] = company
+
+    except Company.DoesNotExist:
+        context_dict['company'] = None
+
+    return render(request, 'company/companies.html', context=context_dict)
+
+
