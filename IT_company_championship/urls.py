57fcc245c3c5221e5f658b48b41dc1335d560821
GUANGHUIMO <frankguanghui@gmail.com>
2020-02-18 17:10:18 +0000

    view_companies

--- IT_company_championship/urls.py
+++ IT_company_championship/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from rango import views
+from company import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatters = [
 	path(' ', views.homepage , name = 'homepage'),
-	path('company/', include('company.urls')),
+	path('view_companies/', include('company.urls')),
 	path('admin/', admin.site.urls),
 ]
