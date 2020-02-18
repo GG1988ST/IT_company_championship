e14faddd2035a0b279eea7cced56fab292226e60
GG1988ST <1078793292@qq.com>
2020-02-18 17:22:13 +0000

    add add_company

--- company/views.py
+++ company/views.py
@@ -18,3 +18,20 @@ def show_company(request, company_name_slug):
     return render(request, 'company/companies.html', context=context_dict)
 
 
+
+def add_company(request):
+    form = CompanyForm()
+
+    if request.method == 'POST':
+        form = CompanyForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return render(request, 'company/index.html')
+
+        else:
+            print(form.errors)
+
+    return render(request, 'company/add_company.html', {'form': form})
+
+
