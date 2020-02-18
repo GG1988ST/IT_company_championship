18bac51f4afb6e2adda4fa750c1920f5130c9112
GUANGHUIMO <frankguanghui@gmail.com>
2020-02-18 16:57:34 +0000

    projecUrls

--- IT_company_championship/urls.py
+++ IT_company_championship/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatters = [
+	path(' ', views.homepage , name = 'homepage'),
+	path('company/', include('company.urls')),
+	path('admin/', admin.site.urls),
 ]
