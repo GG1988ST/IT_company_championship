332ed6178f0266bec7f1e4cd9fb57a69dd52500d
1534959228@qq.com <1534959228@qq.com>
2020-03-03 16:29:46 +0000

    admin.py error

--- Ratecompany/admin.py
+++ Ratecompany/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from Ratecompany.models import Category,Company,Comments
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = {'slug':('name',),'rates','location'}
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('comments','date')
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Comments,CommentsAdmin)
+
