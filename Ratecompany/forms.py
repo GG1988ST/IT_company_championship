3da54e89b285464bc4d08761a3bc6b7141093162
1534959228@qq.com <1534959228@qq.com>
2020-03-15 21:13:39 +0000

    forms

--- Ratecompany/forms.py
+++ Ratecompany/forms.py
@@ -3,11 +3,17 @@ from Ratecompany.models import Company, Comments, Category
 from django.contrib.auth.models import User
 
 
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length= Category.TAB_MAX_LENGTH, help_text="Please enter the category name.")
+    slug= forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = Category
+        fields = ('name',)
+        
 class CompanyForm(forms.ModelForm):
-    name = forms.CharField(max_length=Company.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Company.NAME_MAX_LENGTH, help_text="Please enter the company name.")
     location = forms.CharField(max_length=Company.NAME_MAX_LENGTH,)
     rates = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
-    likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
     slug= forms.CharField(widget=forms.HiddenInput(), required=False)
 
     #for additional information on the form nested class
@@ -15,16 +21,17 @@ class CompanyForm(forms.ModelForm):
         # provide on association between the ModelForm and a model
         model = Company
         # This means specifying only the name field?
-        fields = ('name','location')
+        exclude = ('category',)
 
 
-class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length= Category.TAB_MAX_LENGTH, help_text="Please enter the category name.")
 
+class CommentsForm(forms.ModelForm):
+    comments = forms.CharField(max_length=Comments.COMMENTS_MAX_LENGTH, help_text="Please enter the company name.")
+    date=forms.DateField()
     class Meta:
-        model = Category
-        fields = ('name',)
-
+    # provide on association between the ModelForm and a model
+        model = Comments
+        exclude = ('company',)
 
 
 
