d7071218bf3c2be175b5806ac2f7f6db29049cba
GUANGHUIMO <frankguanghui@gmail.com>
2020-03-16 16:44:16 +0000

    change

--- Ratecompany/views.py
+++ Ratecompany/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, reverse, redirect
-
+from django.contrib.auth import authenticate, login
 from Ratecompany.forms import CompanyForm,Comments
 from Ratecompany.models import Company, Category
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from Ratecompany.forms import UserForm, UserProfileForm
+from django.contrib.auth import logout
+from datetime import datetime
 
 def index(request):
     context_dict = {}
@@ -30,7 +34,7 @@ def show_company(request, company_name_slug):
 
     try:
         company = Company.objects.get(slug=company_name_slug)
-        comments = Comments.objects.filter(company=company)
+        comments = comment.objects.filter(company=company)
 
         context_dict['companies'] = company
         context_dict['comments'] = comments
@@ -80,3 +84,66 @@ def add_comment(request, company_name_slug):
 
     context_dict = {'form': form, 'company': company}
     return render(request, 'Ratecompany/add_comment.html', context=context_dict)
+
+
+
+
+#user register
+
+def register(request):
+    registered = False
+    if request.method=='POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user=user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile=profile_form.save(commit=False)
+            profile.user=user
+
+            if 'picture' in request.FILES:
+                profile.picture =request.FILES['picture']
+
+            profile.save()
+            registered=True
+        else:
+            print(user_form.errors,profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    context_dic ={'user_form':user_form,
+                  'profile_form':profile_form,
+                  'registered':registered}
+
+    return render(request,'Ratecompany/register.html',context=context_dic)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect(reverse('Ratecompany/index.html'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request,'Ratecompany/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('Ratecompany/index.html'))
+
