afa23645111f561f171d61888d28f5887f95a74e
1534959228@qq.com <1534959228@qq.com>
2020-03-16 15:50:09 +0000

    home page

--- Ratecompany/urls.py
+++ Ratecompany/urls.py
@@ -3,5 +3,6 @@ from Ratecompany import views
 
 app_name = 'Ratecompany'
 urlpatterns = [
+    path('',views.index,name='index'),
     path('Company/', views.show_category, name='show_category'),
 ]
