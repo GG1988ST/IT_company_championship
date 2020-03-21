31c86c74ef049aea2921c9668049ef1f26fcee34
GUANGHUIMO <frankguanghui@gmail.com>
2020-03-21 21:19:45 +0000

    populate

--- populate_company.py
+++ populate_company.py
@@ -11,29 +11,29 @@ from Ratecompany.models import Category, Comments, Company
 def populate():
     comment01 = [
         {'comments': 'good job?',
-         'date': '2020-1-3'},
+         'date': '2020-1-3', 'classify':1, 'score': 3},
         {'comments': 'good job>',
-         'date': '2020-2-3'},
+         'date': '2020-2-3','classify':2, 'score': 3},
         {'comments': 'good job{',
-         'date': '2020-3-3'}]
+         'date': '2020-3-3','classify':3, 'score': 4}]
 
     comment02 = [
         {'comments': 'good jobp',
-         'date': '2020-1-1'},
+         'date': '2020-1-1','classify':1, 'score': 5},
         {'comments': 'good job/',
-         'date': '2020-4-2'},
+         'date': '2020-4-2','classify':1, 'score': 3},
         {'comments': 'good job:',
-         'date': '2020-5-3'}]
+         'date': '2020-5-3','classify':1, 'score': 3}]
 
     comment03 = [
         {'comments': 'good jobi',
-         'date': '2020-7-6'},
+         'date': '2020-7-6', 'classify':1, 'score': 3},
         {'comments': 'good job?',
-         'date': '2020-11-7'}]
+         'date': '2020-11-7','classify':3, 'score': 3}]
 
-    itcompany = [{'name': 'Baidu', 'comments': comment01, 'location': 'China', 'rates': 4},
-                 {'name': 'Google', 'comments': comment02, 'location': 'British', 'rates': 2}, ]
-    gamecompany = [{'name': 'Fire', 'comments': comment03, 'location': 'Japan', 'rates': 1}, ]
+    itcompany = [{'name': 'Baidu', 'comments': comment01, 'location': 'China'},
+                 {'name': 'Google', 'comments': comment02, 'location': 'British'}, ]
+    gamecompany = [{'name': 'Fire', 'comments': comment03, 'location': 'Japan'}, ]
 
     category = {'IT': {'company': itcompany},
                 'Game': {'company': gamecompany}}
@@ -41,37 +41,37 @@ def populate():
     for cate, category_data in category.items():
         c = add_category(cate)
         for p in category_data['company']:
-            o = add_company(c, p['name'], p['location'], p['rates'])
+            o = add_company(c, p['name'], p['location'])
             for com in p['comments']:
-                add_comments(o, com['comments'], com['date'])
+                add_comments(o, com['comments'], com['date'], com['classify'], com['score'])
 
     add_category('lalala')
 
-    add_company(add_category('Game'), 'Wangyi', 'GZ', 999)
+    add_company(add_category('Game'), 'Wangyi', 'GZ')
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Company.objects.filter(category=c):
             print(f'- {c}: {p}')
 
 
-def add_comments(company, comments, date):
+def add_comments(company, comments, date,classify, score):
     print(comments)
     print(date)
-    p = Comments.objects.create(company=company, comments=comments,date=date)
+
+    p = Comments.objects.create(company=company, comments=comments,date=date,  classify=classify, score=score)
     return p
 
 
-def add_company(category, name, location, rates):
+def add_company(category, name, location):
 
     c = Company.objects.filter(name=name)
     if c:
         c = c[0]
         c.category = category
         c.location = location
-        c.rates = rates
         c.save()
     else:
-        c = Company.objects.create(category=category, name=name, location=location,rates=rates)
+        c = Company.objects.create(category=category, name=name, location=location)
     #c.category = category
     #c.location = location
     #c.rates = rates
@@ -88,5 +88,3 @@ if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
 
-
-###################
