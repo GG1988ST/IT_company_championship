a47e44f3908bffbf13a0cb01b3b735aac0db11c6
GG1988ST <1078793292@qq.com>
2020-03-21 17:10:05 +0000

    login required

--- Ratecompany/views.py
+++ Ratecompany/views.py
@@ -87,5 +87,66 @@ def add_comment(request, company_name_slug):
 
 
 
+#user register
 
+def register(request):
+    registered = False
+    if request.method=='POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user=user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile=profile_form.save(commit=False)
+            profile.user=user
+
+            if 'picture' in request.FILES:
+                profile.picture =request.FILES['picture']
+
+            profile.save()
+            registered=True
+        else:
+            print(user_form.errors,profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    context_dic ={'user_form':user_form,
+                  'profile_form':profile_form,
+                  'registered':registered}
+
+    return render(request,'Ratecompany/register.html',context=context_dic)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect(reverse('Ratecompany/index.html'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request,'Ratecompany/login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('Ratecompany/index.html'))
+
+
+
+###wqeqwe
 
