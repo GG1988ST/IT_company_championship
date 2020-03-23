569d2141891ae5e9c3fda0b282bc6b107f2216c6
GG1988ST <1078793292@qq.com>
2020-03-23 05:47:08 +0000

    update import forms

--- Ratecompany/views.py
+++ Ratecompany/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import authenticate, login, logout
-from Ratecompany.forms import CompanyForm,Comments
 from Ratecompany.models import Company, Category, Comments, UserProfile
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from Ratecompany.forms import UserForm, UserProfileForm
 from django.contrib.auth import logout
 from datetime import datetime
 from django.db.models import Q
